a0cb4a4a3c1d67cbf5fae8150c8a6e6aa6697394
anushan1508 <anushan1508@gmail.com>
2021-09-07 02:45:44 +0530

    tinder finished

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,50 +1,64 @@
 import express from "express";
 import mongoose from "mongoose";
-import dbCards from "./dbCards";
+import Cards from "./dbCards.js";
+import Cors from "cors";
 
-// import cards from "./models/dbCards";
-
-//App Config
+// App Config
 const app = express();
-const port = process.env.PORT || 8001;
-const coneection_url =
+const port = process.env.PORT || 8001; // port app gonna listen
+const connection_url =
   "mongodb+srv://admin:JXpj0k7A0WOa4UEC@cluster0.7dgpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-//Middlewares
+// Middlewares
+app.use(express.json());
+app.use(Cors());
 
-//DB config
-mongoose.connect(coneection_url, {
+// DB config
+mongoose.connect(connection_url, {
+  // pass in couple of parameters to our connection, make our connection smooth
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
-//API Endpoints
-app.get("/", (req, res) => res.status(200).send("Hello Tinder"));
-// app.post("/tinder/cards", (req, res) => {
-//   const dbCard = req.body;
+// API Endpoints
+// ## go root url, callback func
+app.get("/", (req, res) => {
+  res.status(200).send("HELLO WORLD!!!");
+});
 
-//   Cards.create(dbCard, (err, data) => {
-//     if (err) {
-//       res.statusCode(500).send(err);
-//     } else {
-//       res.statusCode(201).send(data);
-//     }
-//   });
-// });
-// app.get(
-//   "/tinder/cards",
-//   (req,
-//   (res) => {
-//     Cards.find((err, data) => {
-//       if (err) {
-//         res.statusCode(500).send(err);
-//       } else {
-//         res.statusCode(200).send(data);
-//       }
-//     });
-//   })
-// );
+// add data to db, endpoint /tinder/card
+app.post("/tinder/cards", (req, res) => {
+  // save request body into a var
+  const dbCard = req.body;
+  //   function to create a new document
+  Cards.create(dbCard, (err, data) => {
+    // if there is error
+    if (err) {
+      // set response to 500, which means internal server error and send error back
+      res.status(500).send(err);
+    } else {
+      // 201 means created, successfully created our data and send back the data
+      res.status(201).send(data);
+    }
+  });
+});
 
-//Listener
-app.listen(port, () => console.log(`Listening on localhost: ${port}`));
+// another endpoint (the same) which will download data from the db
+// with this will be retrieving every single thing from the collection DB that we just created
+app.get("/tinder/cards", (req, res) => {
+  //   function to find a card
+  Cards.find((err, data) => {
+    // if there is error
+    if (err) {
+      // set response to 500, which means internal server error and send error back
+      res.status(500).send(err);
+    } else {
+      // 200 means found
+      res.status(200).send(data);
+    }
+  });
+});
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
